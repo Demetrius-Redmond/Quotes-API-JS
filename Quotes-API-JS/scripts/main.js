86825ca3ea1960b1563fb8228fdd52c7c48b4e11
Demetrius Redmond <demetriusredmondofl@outlook.com>
2024-08-30 13:08:14 -0500

    completed

--- Quotes-API-JS/scripts/main.js
+++ Quotes-API-JS/scripts/main.js
@@ -1,7 +1,40 @@
 const btnEl = document.getElementById("btn")
+const quoteEl = document.getElementById("quotes")
 
-function getQuote(){
-    console.log("clicked");
+const apiKey = "tzZ8p3IyF/CQ3a+baum6xg==GyuDa8k6FFznvvOu"
+
+const options = {
+    method: "GET", 
+    headers: {
+        "X-Api-KEY": apiKey,
+    },
+
+};
+var category = 'inspirational'
+const apiURL = "https://api.api-ninjas.com/v1/quotes?category=" + category
+
+async function getQuote(){
+    
+    try {
+        quoteEl.innerText = "Updating...";
+    btnEl.ariaDisabled = true;
+    btnEl.innerText = "Loading..."
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.ariaDisabled = false;
+    btnEl.innerText = "Show me a new quote."
+
+    quoteEl.innerText = data[0].quote;
+    } catch (error) {
+        quoteEl.innerText = "An error occured, try again later."
+        btnEl.ariaDisabled = false;
+        btnEl.innerText = "Show me a new quote."
+        console.log(error);
+        
+    }
+
+    
 }
 
 btnEl.addEventListener("click", getQuote)
@@ -9,7 +42,7 @@ btnEl.addEventListener("click", getQuote)
 
 
 
-// var category = 'happiness'
+// 
 // $.ajax({
 //     method: 'GET',
 //     url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
